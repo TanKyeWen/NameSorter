a61913c5f2d2bdb9635dd142654b20126d244b22
TanKyeWen <tkyewen@gmail.com>
2024-06-21 11:45:28 +0800

    commit

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import pandas as pd
 import numpy as np
 
+#file location
 file_loc = "D:\\Self\\NameSorter\\text.xlsx"
 
-studentList = pd.read_excel('D:/Self/NameSorter/test.xlsx')
+#Use Pandas Framework to get all Data from Excel to a tuple
+studentList = pd.read_excel('D:/Project/NameSorter/test.xlsx')
 
+#Filter out non Facilitators
 facilitatorsList = studentList[studentList['Position'] == 'Facilitators']
 
-
 #Sort Experienced Facilitators by Gender
-experiencedFacilitatorsMale = facilitatorsList[(facilitatorsList['Experience'] == "Yes") & (facilitatorsList["Gender"] == "M")]
+experiencedFacilitatorsMale = facilitatorsList[(facilitatorsList['Experience'] == "Yes") & (facilitatorsList["Gender"] == "M")].reset_index(drop = True)
 experiencedFacilitatorsFemale = facilitatorsList[(facilitatorsList['Experience'] == "Yes") & (facilitatorsList["Gender"] == "F")]
 
 #Sort Inexperienced Facilitators by Gender
 inexperiencedFacilitatorsMale = facilitatorsList[(facilitatorsList['Experience'] == "No") & (facilitatorsList["Gender"] == "M")]
 inexperiencedFacilitatorsFemale = facilitatorsList[(facilitatorsList['Experience'] == "No") & (facilitatorsList["Gender"] == "F")]
 
-print(facilitatorsList)
-print(experiencedFacilitatorsMale)
+#Randomise Facilitators to groups based on Experience and Gender
