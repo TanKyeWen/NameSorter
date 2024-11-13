954de968c0d450e4b01eadb2bea9779840b9c2e2
TanKyeWen <tkyewen@gmail.com>
2024-11-13 13:34:15 +0800

    commit

--- app.py
+++ app.py
@@ -1,53 +1,81 @@
 import pandas as pd
-import random
+import numpy as np
+from openpyxl import load_workbook
+import os
 
-# TO get user inputs
-def main():
-    print("aa")
+print("_" * 50)
+print("Welcome To Name Sorter!")
+print("_" * 50)
+print("* Note: THE EXCEL FILE MUST BE IN THE SAME FOLDER AS THIS PYTHON FILE!!")
 
-# Append user to group 
-def groupList(specificList, num, startRow, startCol):
-    facilitator = pd.DataFrame()
-    facilitator = pd.concat([facilitator, pd.DataFrame(specificList.loc[num]).T])
+fileName = input("Enter your Excel Name: \n")
+outputFileName = input("Enter the output file name: \n")
+roleColomnName = input("Enter the Roles Column Name: \n")
+experienceColumnName = input("Enter the Experience Column name: \n")
+faciIdentifier = input("Enter the Facilitator's role name : \n")
 
-    specificList.drop(num, inplace = True)
-    specificList.reset_index(drop = True, inplace = True)
+#To know if foundation and degree are seperated or not
+seperationInput = input("Do you want to seperate Foundation and Degree? (Y/N)\n").capitalize()
 
-    facilitator.to_excel('ModifiedTest.xlsx', index_label = False, startrow = startRow, startcol = startCol)
+seperation = 2 if seperationInput in ['Y', 'YES'] else 1
+if seperation == 2:
+    studiesColumnName = input("Enter the Degree and Foundation Column name: \n")
+    degreeIdentifier = input("Enter the Degree's role name: \n")
+    numOfGrpDegree = int(input("Enter the Numbers of Groups for Degree: \n"))
+    foundationIdentifier = input("Enter the Foundation's role name: \n")
+    numOfGrpFoundation = int(input("Enter the Numbers of Groups for Foundation: \n"))
+else:
+    numOfGrpDegree = int(input("Enter the Numbers of Groups for Degree: \n"))
 
 #file location
-file_loc = "./text.xlsx"
+fileLoc = os.getcwd() + "/" + (fileName if fileName.endswith(".xlsx") else fileName + ".xlsx")
 
 #Use Pandas Framework to get all Data from Excel to a tuple
-studentList = pd.read_excel('./test.xlsx')
+studentList = pd.read_excel(fileLoc)
 
-#Filter out non Facilitators
-facilitatorsList = studentList[studentList['Position'] == 'Facilitators']
+destinationFilePath = os.getcwd() + '/' + outputFileName + '.xlsx'
+studentList.to_excel(destinationFilePath, sheet_name='AllData', index=False)
 
-#Sort Experienced Facilitators by Gender
-experiencedFacilitatorsMale = facilitatorsList[(facilitatorsList['Experience'] == "Yes") & (facilitatorsList["Gender"] == "M")].reset_index(drop = True)
-experiencedFacilitatorsFemale = facilitatorsList[(facilitatorsList['Experience'] == "Yes") & (facilitatorsList["Gender"] == "F")].reset_index(drop = True)
+def create_balancedGrps(df, numOfGrps):
+    # Stratify by Gender and Experience
+    strata = df.groupby(['Gender', experienceColumnName])
+    
+    # Initialize empty groups
+    groups = {i: pd.DataFrame(columns=df.columns) for i in range(numOfGrps)}
+    
+    # Shuffle and distribute each stratum
+    for _, group in strata:
+        group = group.sample(frac=1).reset_index(drop=True)  # Shuffle
+        for i in range(len(group)):
+            groups[i % numOfGrps] = pd.concat([groups[i % numOfGrps], group.iloc[[i]]])
 
-#Sort Inexperienced Facilitators by Gender
-inexperiencedFacilitatorsMale = facilitatorsList[(facilitatorsList['Experience'] == "No") & (facilitatorsList["Gender"] == "M")].reset_index(drop = True)
-inexperiencedFacilitatorsFemale = facilitatorsList[(facilitatorsList['Experience'] == "No") & (facilitatorsList["Gender"] == "F")].reset_index(drop = True)
+    groups[numOfGrps - 1] = pd.concat([groups[numOfGrps - 1], groups[0].iloc[[-1]]])
+    groups[numOfGrps - 2] = pd.concat([groups[numOfGrps - 1], groups[0].iloc[[-1]]])
+    
+    return groups
 
-memberPerGroup = 1
-numOfGrp = 2
+def toExcel(combinedDataframe, balancedGroups, sheet):
+    for i, group in balancedGroups.items():
+        if combinedDataframe.empty:
+            combinedDataframe = group.reset_index(drop=True)
+        else:
+            combinedDataframe = pd.concat([combinedDataframe, pd.DataFrame(np.nan, index=combinedDataframe.index, columns=[''])], axis=1)
+            combinedDataframe = pd.concat([combinedDataframe, group.reset_index(drop=True)], axis=1)
+    
+    # Write the combined DataFrame to an Excel file
+    with pd.ExcelWriter(destinationFilePath, engine='openpyxl', mode='a') as writer:
+        combinedDataframe.to_excel(writer, sheet_name=sheet, index=False)
 
-for y in range(numOfGrp):
-    for i in range(memberPerGroup):
-        rng = random.randint(0, len(experiencedFacilitatorsFemale))
-        groupList(experiencedFacilitatorsFemale, rng, 0, y)
+for _ in range(seperation):
+    #Filter out non Facilitators
+    facilitatorsList = studentList[(studentList[roleColomnName] == faciIdentifier) if seperation == 1 else
+                                   ((studentList[roleColomnName] == faciIdentifier) & 
+                                    (studentList[studiesColumnName] == (foundationIdentifier if _ == 0 else degreeIdentifier)))].reset_index(drop = True)
 
-    for i in range(memberPerGroup):
-        rng = random.randint(0, len(experiencedFacilitatorsMale))
-        groupList(experiencedFacilitatorsMale, rng, memberPerGroup, y)
+    # Create balanced groups
+    balancedGrps = create_balancedGrps(facilitatorsList, (numOfGrpFoundation if _ == 0 else numOfGrpDegree))
 
-    for i in range(memberPerGroup): 
-        rng = random.randint(0, len(inexperiencedFacilitatorsFemale))
-        groupList(inexperiencedFacilitatorsFemale, rng, memberPerGroup*2, y)
+    # Combine groups into one DataFrame with each group separated by one column
+    combinedDataframe = pd.DataFrame()
 
-    for i in range(memberPerGroup):
-        rng = random.randint(0, len(inexperiencedFacilitatorsMale))
-        groupList(inexperiencedFacilitatorsMale, rng, memberPerGroup*3, y)
+    toExcel(combinedDataframe, balancedGrps, ('FoundationFaci' if _ == 0 else 'DegreeFaci'))
